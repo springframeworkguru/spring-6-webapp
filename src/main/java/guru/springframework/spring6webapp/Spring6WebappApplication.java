52f13621f078d571b264b3da203bc7a86cdb5595
John Thompson <john@springframework.guru>
2022-07-20 15:17:14 -0400

    Initial add of project

---
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6WebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
