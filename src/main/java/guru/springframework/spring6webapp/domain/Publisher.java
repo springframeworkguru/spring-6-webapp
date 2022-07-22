ce255868d53d06679b6a351fecede04a159aa127
John Thompson <john@springframework.guru>
2022-07-22 15:41:04 -0400

    adding publisher relationship

--- src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring6webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -20,6 +19,9 @@ public class Publisher {
     private String city;
     private String state;
     private String zipCode;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
 
     public Long getId() {
         return id;
