0c547bf4a520d8f1c66b52510a5f790b326d5e4c
John Thompson <john@springframework.guru>
2022-07-24 08:05:02 -0400

    adding book service layer

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
