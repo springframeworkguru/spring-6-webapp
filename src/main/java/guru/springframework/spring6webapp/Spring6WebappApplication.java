d2363a885284d9c1e4bd4c0897e8164d55dc4819
Sandeep Sankranti <sankranti@Sandeeps-MacBook-Pro-2.local>
2023-06-09 16:36:03 -0700

    test commit

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring6WebappApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello");
         SpringApplication.run(Spring6WebappApplication.class, args);
     }
 
