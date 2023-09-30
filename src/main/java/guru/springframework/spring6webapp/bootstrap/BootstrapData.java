b257b0983a213d85d10d020cf260618f62fd190e
Tzur1234 <tzur09@gmail.com>
2023-09-30 17:27:58 +0300

    Add new "moo moo"

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class BootstrapData implements CommandLineRunner {
 
     // Bla bla
+    // moo moo
 
 
     private final AuthorRepository authorRepository;
