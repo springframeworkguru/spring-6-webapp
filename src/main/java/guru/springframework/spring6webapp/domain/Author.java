5324b3749332d61346fdd87703121e4eabc24489
beheidary <behnam.b.heidary@gmail.com>
2024-01-10 12:00:19 +0330

    hibernate equity for terminate same entities

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -47,4 +47,36 @@ public class Author {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
+
+
+
+
+
+
