ed81f2debf7892ec85a27677ff1074022829ee74
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-03-30 18:17:48 +1100

    Added book service

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookService.java
@@ -0,0 +1,5 @@
+package guru.springframework.spring6webapp.services;/**
+* @Author tmekaumput
+* @Date 30/3/24 5:47 pm
+*/public interface BookService {
+}
---
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,5 @@
+package guru.springframework.spring6webapp.services;/**
+* @Author tmekaumput
+* @Date 30/3/24 5:51 pm
+*/public class BookServiceImpl {
+}
