7ae61653ed11f18fba0ef454bf89d40566a06e17
Casper Hooft <casper.hooft@ordina.nl>
2024-04-02 12:14:19 +0200

    Hi ho

---
+++ src/main/java/guru/springframework/spring6webapp/Domain/Author.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring6webapp.Domain;
+
+    import jakarta.persistence.*;
+
+            import java.util.Set;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
