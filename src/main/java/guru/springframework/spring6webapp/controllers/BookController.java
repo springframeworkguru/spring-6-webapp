b760ed5b20886450c29345d424156ed878f6275a
Casper Hooft <casper.hooft@ordina.nl>
2024-04-09 16:39:15 +0200

    Midway Section 2 video 26
    Fixed all changes the author did off screen between video 25 and 26 without showing...
    
    Signed-off-by: Casper Hooft <casper.hooft@ordina.nl>

---
+++ src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6webapp.controllers;
+
+import guru.springframework.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookService.findAll());
+
+        return "books";
+    }
+
+}
