f419d1bea2de4cb32303c6c93d9ccfab32db7122
bgrcdnc <bugracaliskandinc@gmail.com>
2024-08-24 17:24:57 +0300

    Add: Bootstrap init

---
+++ src/main/java/com/bugracdnc/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,13 @@
+package com.bugracdnc.spring6webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
