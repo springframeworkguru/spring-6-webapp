90e0e9eb34156033b7224026ecc67573c56b8937
bgrcdnc <bugracaliskandinc@gmail.com>
2024-08-24 18:35:24 +0300

    Bugfix: Author to Book relation

--- src/main/java/com/bugracdnc/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/bugracdnc/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.bugracdnc.spring6webapp.bootstrap;
 
 import com.bugracdnc.spring6webapp.domain.Author;
 import com.bugracdnc.spring6webapp.domain.Book;
+import com.bugracdnc.spring6webapp.domain.Publisher;
 import com.bugracdnc.spring6webapp.repositories.AuthorRepository;
 import com.bugracdnc.spring6webapp.repositories.BookRepository;
+import com.bugracdnc.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,35 +14,36 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Author oguzatay = new Author();
-        oguzatay.setFirstName("Oguz");
-        oguzatay.setLastName("Atay");
+    public void run(String... args) {
+        Author oguzatay = authorRepository.save(new Author("Oguz", "Atay"));
+        Book tutunamayanlar = bookRepository.save(new Book("Tutunamayanlar", "123456"));
+        Book tehlikeliOyunlar = bookRepository.save(new Book("Tehlikeli Oyunlar", "123457"));
+        Publisher iletisim = publisherRepository.save(new Publisher("Iletisim"));
 
-        Book tutunamayanlar = new Book();
-        tutunamayanlar.setTitle("Tutunamayanlar");
-        tutunamayanlar.setIsbn("123456");
+        tutunamayanlar.setPublisher(iletisim);
+        tehlikeliOyunlar.setPublisher(iletisim);
 
-        Book tehlikeliOyunlar = new Book();
-        tehlikeliOyunlar.setTitle("Tehlikeli Oyunlar");
-        tehlikeliOyunlar.setIsbn("123457");
+        oguzatay.getBooks().add(tutunamayanlar);
+        oguzatay.getBooks().add(tehlikeliOyunlar);
+        tutunamayanlar.getAuthors().add(oguzatay);
+        tehlikeliOyunlar.getAuthors().add(oguzatay);
+        authorRepository.save(oguzatay);
+        bookRepository.save(tutunamayanlar);
+        bookRepository.save(tehlikeliOyunlar);
 
-        Author oguzataySaved = authorRepository.save(oguzatay);
-        Book tutunamayanlarSaved = bookRepository.save(tutunamayanlar);
-        Book tehlikeliOyunlarSaved = bookRepository.save(tehlikeliOyunlar);
-
-        oguzataySaved.getBooks().add(tutunamayanlarSaved);
-        oguzataySaved.getBooks().add(tehlikeliOyunlarSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
