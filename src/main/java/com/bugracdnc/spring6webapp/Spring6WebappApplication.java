aba6cd332eba7996b7d33dcf947da599508a5361
bgrcdnc <bugracaliskandinc@gmail.com>
2024-08-24 16:51:59 +0300

    Update: Package name

--- src/main/java/com/bugracdnc/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/com/bugracdnc/spring6webapp/Spring6WebappApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring6webapp;
+package com.bugracdnc.spring6webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
