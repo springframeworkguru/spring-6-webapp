55373169de6b93b2b23652f7773486327a04e491
bgrcdnc <bugracaliskandinc@gmail.com>
2024-08-24 22:31:59 +0300

    Add: Book and Author Controllers

---
+++ src/main/java/com/bugracdnc/spring6webapp/controllers/BookController.java
@@ -0,0 +1,22 @@
+package com.bugracdnc.spring6webapp.controllers;
+
+import com.bugracdnc.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+}
