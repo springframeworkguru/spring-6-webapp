0bb0302747da6a2a8454f320911f03110b714798
bgrcdnc <bugracaliskandinc@gmail.com>
2024-08-24 17:21:33 +0300

    Add: Repository template

--- src/main/java/com/bugracdnc/spring6webapp/repositories/AuthorRepository.java
+++ src/main/java/com/bugracdnc/spring6webapp/repositories/AuthorRepository.java
@@ -1,6 +1,7 @@
 package com.bugracdnc.spring6webapp.repositories;
 
+import com.bugracdnc.spring6webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthorRepository extends CrudRepository {
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/com/bugracdnc/spring6webapp/repositories/BookRepository.java
+++ src/main/java/com/bugracdnc/spring6webapp/repositories/BookRepository.java
@@ -1,6 +1,7 @@
 package com.bugracdnc.spring6webapp.repositories;
 
+import com.bugracdnc.spring6webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository {
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
