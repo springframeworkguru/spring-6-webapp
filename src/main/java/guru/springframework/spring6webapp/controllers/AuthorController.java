d08277f2d23639dabf5bcd02fcdaf513055fc042
davidrodecap <rodecapd@objectcomputing.com>
2025-01-01 09:38:12 -0600

    Assignment 2

---
+++ src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6webapp.controllers;
+
+import guru.springframework.spring6webapp.services.AuthorService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorService.findAll());
+        return "authors";
+    }
+}
---
+++ src/main/java/guru/springframework/spring6webapp/services/AuthorServiceImpl.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+    private AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+
+    @Override
+    public Iterable<Author> findAll() {
+        return this.authorRepository.findAll();
+    }
+}
